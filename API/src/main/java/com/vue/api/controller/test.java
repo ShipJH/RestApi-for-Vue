3102f549097e8e0b3cfbfe1ec414e57b69a2e8c2
ShipJH <ship_reappearance@naver.com>
2018-06-04 22:57:44 +0900

    db접속정보 변경

--- API/src/main/java/com/vue/api/controller/test.java
+++ API/src/main/java/com/vue/api/controller/test.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,14 @@ import com.vue.api.responseVo.ReqTestVo;
 import com.vue.api.service.TestService;
 import com.vue.api.vo.TestVo;
 
+
 @RestController
 public class test {
 
 	@Autowired
 	private TestService testService;
 	
+	@CrossOrigin
 	@GetMapping(value="/test")
 	public ResponseEntity<TestVo> testm(){
 		
