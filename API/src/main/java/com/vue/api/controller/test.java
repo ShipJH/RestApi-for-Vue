92042e2062349837e984001425c74ebef9981b54
yuhyunjae12 <yuhyunjae12@naver.com>
2018-06-04 19:19:20 +0900

    db 접속정보 변경

--- API/src/main/java/com/vue/api/controller/test.java
+++ API/src/main/java/com/vue/api/controller/test.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class test {
 	@Autowired
 	private TestService testService;
 	
+	@CrossOrigin
 	@GetMapping(value="/test")
 	public ResponseEntity<TestVo> testm(){
 		
