40270b03cd29ec3482744011e54da88ade013895
yuhyunjae12 <yuhyunjae12@naver.com>
2018-06-10 15:38:50 +0900

    restApi Swagger기본 적용

--- API/src/main/java/com/vue/api/ApiApplication.java
+++ API/src/main/java/com/vue/api/ApiApplication.java
@@ -1,14 +1,35 @@
 package com.vue.api;
 
+import static springfox.documentation.builders.PathSelectors.regex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import com.google.common.base.Predicate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @ComponentScan
+@EnableSwagger2
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.ant("/*")) // 그중 /* 인 URL들 필터링
+                .build();
+    }
+
 }
