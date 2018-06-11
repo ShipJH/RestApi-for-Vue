e4b29bba8a7e2aa36ac5eb3e86438a55e8e4ee3d
ShipJH <ship_reappearance@naver.com>
2018-06-11 10:38:29 +0900

    .

--- API/src/main/java/com/vue/api/ApiApplication.java
+++ API/src/main/java/com/vue/api/ApiApplication.java
@@ -1,13 +1,26 @@
 package com.vue.api;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.base.Predicate;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -29,5 +42,55 @@ public class ApiApplication {
                 /*.paths(PathSelectors.any())*/	// 모든 맵핑한걸 다잡는거같아요~!
                 .build();
     }
-
+    
+//    public Docket config(String groupName, Predicate path) {
+//        return new Docket(DocumentationType.SWAGGER_2).groupName(groupName).useDefaultResponseMessages(false)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.vue.api.controller"))
+//                .paths(path)
+////                .paths(PathSelectors.ant("/**"))
+//                .build().directModelSubstitute(LocalDate.class, String.class)
+//                .globalResponseMessage(RequestMethod.GET,
+//                        Arrays.asList(new ResponseMessageBuilder()
+//                                        .code(500)
+//                                        .message("서버 오류!")
+//                                        .build(),
+//                                new ResponseMessageBuilder()
+//                                        .code(401)
+//                                        .message("인증정보가 필요합니다 !")
+//                                        .build()))
+//                .globalResponseMessage(RequestMethod.POST,
+//                        Arrays.asList(new ResponseMessageBuilder()
+//                                        .code(500)
+//                                        .message("서버 오류!")
+//                                        .build(),
+//                                new ResponseMessageBuilder()
+//                                        .code(401)
+//                                        .message("인증정보가 필요합니다 !")
+//                                        .build()))
+//                .genericModelSubstitutes(ResponseEntity.class)
+//                .securitySchemes(Arrays.asList(apiKey()))
+//                .securityContexts(Arrays.asList(securityContext()));
+//
+//    }
+//
+//    private ApiKey apiKey() {
+//        return new ApiKey("apiKey", "Authorization", "header");
+//    }
+//
+//    private SecurityContext securityContext() {
+//        return SecurityContext.builder().securityReferences(defaultAuth())
+//                .forPaths(PathSelectors.any()).build();
+//    }
+//
+//
+//    private List<SecurityReference> defaultAuth() {
+//        AuthorizationScope authorizationScope = new AuthorizationScope(
+//                "global", "accessEverything");
+//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+//        authorizationScopes[0] = authorizationScope;
+//        return Arrays.asList(new SecurityReference("apiKey",
+//                authorizationScopes));
+//    }
+    
 }
