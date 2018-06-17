0b6b9ffedc244090ed2f00f1ae36efe507a060c8
ShipJH <ship_reappearance@naver.com>
2018-06-17 17:50:51 +0900

    변경

--- API/src/main/java/com/vue/api/service/BoardService.java
+++ API/src/main/java/com/vue/api/service/BoardService.java
@@ -2,29 +2,12 @@ package com.vue.api.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.vue.api.mapper.BoardMapper;
-import com.vue.api.service.impl.BoardServiceImpl;
 import com.vue.api.vo.boardVo.BoardVo;
 
-@Service
-public class BoardService implements BoardServiceImpl{
+//import com.vue.api.vo.TestVo;
 
-	@Autowired
-	private BoardMapper boardMapper;
-	
-	@Override
-	public List<BoardVo> findByBoardList() {
-		return boardMapper.findByBoardList();
-	}
-	
-//	@Override
-//	public TestVo getTest() {
-//		 
-//		return testMapper.getTestValue();
-//	}
+public interface BoardService {
 
-	
+	List<BoardVo> findByBoardList();
+//	TestVo getTest(); 
 }
--- API/src/main/java/com/vue/api/service/impl/BoardServiceImpl.java
+++ API/src/main/java/com/vue/api/service/impl/BoardServiceImpl.java
@@ -2,12 +2,29 @@ package com.vue.api.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vue.api.mapper.BoardMapper;
+import com.vue.api.service.BoardService;
 import com.vue.api.vo.boardVo.BoardVo;
 
-//import com.vue.api.vo.TestVo;
+@Service
+public class BoardServiceImpl implements BoardService{
 
-public interface BoardServiceImpl {
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Override
+	public List<BoardVo> findByBoardList() {
+		return boardMapper.findByBoardList();
+	}
+	
+//	@Override
+//	public TestVo getTest() {
+//		 
+//		return testMapper.getTestValue();
+//	}
 
-	List<BoardVo> findByBoardList();
-//	TestVo getTest(); 
+	
 }
